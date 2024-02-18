cd9df3c2a0c1cf7cefc7e93746309d3f81afa808
LaFungus <150188976+LaFungus@users.noreply.github.com>
2024-02-18 16:23:43 +0200

    githuby

---
+++ EpicGame/Assets/Scripts/Morka.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Morka : MonoBehaviour
+{
+    public float morkosSpeed;
+    private Rigidbody2D rb;
+    private Vector2 lastVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lastVelocity = rb.velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        morkosSpeed = lastVelocity.magnitude;
+        Vector2 direction = Vector2.Reflect(lastVelocity.normalized, coll.contacts[0].normal);
+
+        rb.velocity = direction * Mathf.Max(morkosSpeed, 0f);
+    }
+}
