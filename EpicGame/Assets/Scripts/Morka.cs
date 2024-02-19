26c5568dbe91a71d545c5586d8ae90e055789061
LaFungus <150188976+LaFungus@users.noreply.github.com>
2024-02-19 09:41:40 +0200

    llllllllllllllll

--- EpicGame/Assets/Scripts/Morka.cs
+++ EpicGame/Assets/Scripts/Morka.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Morka : MonoBehaviour
 {
-    public float morkosSpeed;
+    private float morkosSpeed =  4f;
     private Rigidbody2D rb;
     private Vector2 lastVelocity;
 
